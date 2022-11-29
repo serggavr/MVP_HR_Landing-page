bd4735a28bd6c39240a0540e5178167ef9c61850
GSergey <g.sergey@bk.ru>
2022-11-29 13:49:26 +0300

    ref: vacancy, button, search-form, vacancy-card, add: vacancies filter & render

---
+++ src/scripts/CreateVacancyCard.js
@@ -0,0 +1,17 @@
+export default function createVacancyCard (vacancyData, vacancyCardTemplateSelector) {
+  const vacancyCard = document.querySelector(vacancyCardTemplateSelector).content.querySelector(".vacancy-card").cloneNode(true)
+  
+  const vacancyName = vacancyCard.querySelector('.vacancy-card__vacancy-name')
+  const vacancyCourse = vacancyCard.querySelector('.vacancy-card__vacancy-course')
+  const vacancyDirection = vacancyCard.querySelector('.vacancy-card__vacancy-direction')
+  const vacancySalary = vacancyCard.querySelector('.vacancy-card__vacancy-salary')
+  const vacancyLink = vacancyCard.querySelector('.vacancy-card__link')
+
+  vacancyName.innerHTML = vacancyData.role
+  vacancyCourse.innerHTML = vacancyData.course
+  vacancyDirection.innerHTML = vacancyData.faculty
+  vacancySalary.innerHTML = vacancyData.sаlary
+  vacancyLink.src = vacancyData.link
+
+  return(vacancyCard)
+}
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,98 @@
 import css from "../pages/index.css";
 
+import { vacanciesList } from '../utils/constants';
+import dropdown from './Dropdown'
+import createVacancyCard from './CreateVacancyCard';
+import Section from './Section.js'
+
 console.log("run index.js");
+
+const vacanciesContainerSelector = '.vacancy__container';
+const cardTemplateSelector = '#vacancyCard';
+const loadMoreVacanciesButtonSelector = '.vacancy__load-more-button';
+const dropdownButtonTypeFacultySelector = '.dropdown__button_type_faculty'
+const dropdownButtonTypeRoleSelector = '.dropdown__button_type_role'
+const dropdownButtonSelectedClass = 'dropdown__button_style_selected'
+
+const loadMoreVacanciesButtonButton = document.querySelector(loadMoreVacanciesButtonSelector)
+const dropdownTypeFacultyButton = document.querySelector(dropdownButtonTypeFacultySelector)
+const dropdownTypeRoleButton = document.querySelector(dropdownButtonTypeRoleSelector)
+
+let filteredVacanciesList = []
+
+const vacanciesCardsList = new Section({
+  renderer: (item) => {
+    const vacancy = createVacancyCard(item, cardTemplateSelector)
+    vacanciesCardsList.addItem(vacancy);
+  }
+}, vacanciesContainerSelector)
+
+loadMoreVacanciesButtonButton.addEventListener('click', () => {
+
+  vacanciesCardsList.renderItems(applyFilters(vacanciesList), 3)
+
+  if (vacanciesCardsList.container.children.length === filteredVacanciesList.length ) {
+    loadMoreVacanciesButtonButton.setAttribute('disabled', '')
+  } else {
+    loadMoreVacanciesButtonButton.removeAttribute('disabled', '')
+  }
+})
+
+const filterVacancies = (query, queryField, vacanciesList) => {
+  let filteredList = vacanciesList
+  if (query === '' || query === 'Все') {
+    return filteredList 
+  }
+  filteredList = vacanciesList.filter(el => el[queryField] === query)
+  return filteredList
+}
+
+dropdownTypeFacultyButton.addEventListener('change', (e) => {
+
+  vacanciesCardsList.removeItems()
+  vacanciesCardsList.renderItems(applyFilters(vacanciesList), 6)
+
+  if (e.target.value === '' || e.target.value === 'Все') {
+    dropdownTypeFacultyButton.classList.remove(dropdownButtonSelectedClass)
+  } else {
+    dropdownTypeFacultyButton.classList.add(dropdownButtonSelectedClass)
+  }
+})
+
+dropdownTypeRoleButton.addEventListener('change', (e) => {
+
+  vacanciesCardsList.removeItems()
+  vacanciesCardsList.renderItems(applyFilters(vacanciesList), 6)
+
+  if (e.target.value === '' || e.target.value === 'Все') {
+    dropdownTypeRoleButton.classList.remove(dropdownButtonSelectedClass)
+  } else {
+    dropdownTypeRoleButton.classList.add(dropdownButtonSelectedClass)
+  }
+})
+
+const applyFilters = (vacanciesList) => {
+  filteredVacanciesList = filterVacancies(dropdownTypeRoleButton.value, 'role', vacanciesList)
+  filteredVacanciesList = filterVacancies(dropdownTypeFacultyButton.value, 'faculty', filteredVacanciesList)
+
+  if (vacanciesCardsList.container.children.length === filteredVacanciesList.length || filteredVacanciesList.length <= 6) {
+    loadMoreVacanciesButtonButton.setAttribute('disabled', '')
+  } else {
+    loadMoreVacanciesButtonButton.removeAttribute('disabled', '')
+  }
+
+  return filteredVacanciesList
+}
+
+vacanciesCardsList.removeItems()
+vacanciesCardsList.renderItems(applyFilters(vacanciesList), 6)
+
+
+
+
+
+
+
+
+
+
