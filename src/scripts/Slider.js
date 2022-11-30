dc79017df61949a67f2513c9abfff524438edb26
GSergey <g.sergey@bk.ru>
2022-12-01 02:12:45 +0300

    add: slider css

--- src/scripts/Slider.js
+++ src/scripts/Slider.js
@@ -1,9 +1,14 @@
 import Splide from '@splidejs/splide';
 import '@splidejs/splide/css';
 
+
 export default () => {
     const splide = new Splide( '.splide', {
-        arrows: false
+        arrows: false,
+        type   : 'loop',
+        padding: { right: '0px' },
+        lazyLoad: 'nearby',
+        wheel    : true,
     } );
     splide.mount();
 
